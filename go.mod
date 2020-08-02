504ee52a3eafd840ac9ced8097ebf090f6e43725
jameszhangshanghai <samoren@126.com>
2020-08-03 07:42:03 +0800

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module testnats
+
+go 1.14
+
+require github.com/nats-io/nats.go v1.10.0
--- main.go
+++ main.go
@@ -2,72 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
-
-	"github.com/nats-io/go-nats"
+        "time"
+	"github.com/nats-io/nats.go"
 )
-
-const (
-	url  = "nats://127.0.0.1:4222"
-	subj = "foo"
-)
-
-var (
-	nc  *nats.Conn
-	err error
-)
-
-func init() {
-	if nc, err = nats.Connect(url); checkErr(err) {
-		//
-	}
+var NatsServerUrl = "mynats:4222"
+//TestSub : test function for sub
+func testSub() {
+	nc, err := nats.Connect(NatsServerUrl)
+	defer func() {
+		fmt.Println("Sub nats connect err: ", err)
+		nc.Close()
+	}()
+        for {
+	    time.Sleep(time.Second)
+	    nc.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+	    })
+         }
 }
 
-//3 mode
-//Publish-Subscribe
-//Request-Reply
-//Queue group
-//Acknowledgements
-
-func main() {
-
-	//test pub sub
-	//go TestPub()
-	time.Sleep(time.Second)
-	//go TestSub()
-
-	//test request reply
-	//nc, _ := nats.Connect(url)
-	//defer nc.Close()
-
-	nc.Subscribe("foo", func(m *nats.Msg) {
-		nc.Publish(m.Reply, []byte("I will help you"))
-	})
-	reply, _ := nc.Request("foo", []byte("help"), 50*time.Millisecond)
-	fmt.Println(string(reply.Data))
-
-	//Queue
-	received := 0
-	nc.QueueSubscribe("foo", "worker_group", func(_ *nats.Msg) {
-		received++
-	})
-
-	//acknowlage
-	nc.Subscribe("foo", func(m *nats.Msg) {
-		m.Respond([]byte(""))
-	})
-
-	reply, _ = nc.Request("foo", []byte("help"), 50*time.Millisecond)
-	fmt.Println("ack:", string(reply.Data))
-	fmt.Println("Main test ended!!")
+func testPub() {
+	nc, err := nats.Connect(NatsServerUrl)
+	defer func() {
+		fmt.Println("Pub nats connect err: ", err)
+		nc.Close()
+	}()
+	for {
+            time.Sleep(time.Second)
+	    nc.Publish("foo", []byte("Hello World!"))
+         }
 
 }
 
-func checkErr(err error) bool {
-	if err != nil {
-		log.Fatal(err)
-		return false
-	}
-	return true
+func main(){
+	go testPub()
+	testSub()
 }
